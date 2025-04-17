35d56a9f929893c581e495a4089c34fa1adf1710
Gennady Kuznetsov <only1exit@gmail.com>
2025-04-18 01:56:06 +0300

    Metrics done, added endpoint + configuried promhttp, made simple dashboard (/grafana)

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -3,10 +3,12 @@ package app
 import (
 	"log"
 
+	echoPrometheus "github.com/globocom/echo-prometheus"
 	"github.com/kuznetsovgg/go-server-practice/internal/app/endpoint"
 	"github.com/kuznetsovgg/go-server-practice/internal/app/middlewares"
 	"github.com/kuznetsovgg/go-server-practice/internal/app/service"
 	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type App struct {
@@ -24,7 +26,10 @@ func New() (*App, error) {
 
 	a.echo = echo.New()
 
+	a.echo.Use(echoPrometheus.MetricsMiddleware())
+
 	a.echo.GET("/", a.e.Check, middlewares.UAcheck)
+	a.echo.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	return a, nil
 }
